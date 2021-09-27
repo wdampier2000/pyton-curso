d3e7d584d6fa4d63345ea34dcdde93d810fa4fe4
wdampier2000 <wdampier2000@gmail.com>
2021-09-27 12:51:50 -0300

    JSON

--- Lectura_Json/main.py
+++ Lectura_Json/main.py
@@ -2,10 +2,12 @@
 import json
 
 def cargar_datos(ruta):
-    with open(ruta) as contenido:
-       cursos = json.load(contenido)
-       print(cursos)
-
+    with open(ruta) as contenido: # ABRIMOS EL ARCHIVO JSON
+       cursos = json.load(contenido) # CONVIERTE EL CONTENIDO A UN FORMATO JSON
+       for curso in cursos:
+            print(curso) # entrega todos los diccionarios
+            print(curso.get('nombre')) # entrega el valor de nombre del diccionario
+            print(curso.get('duracion', '')) # '' = si no existe entrega cadena vacia
 
 if __name__== '__main__':
     ruta = "./Lectura_Json/datos/cursos.json"
